e2cf62eca5d6d4bd5324a8d82105598f61dde986
miraclesprime <miraclesprime@gmail.com>
2025-12-16 08:54:21 -0500

    changed the style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ export default function Header(){
   return (
     <header className="site-header">
       <div className="logo" style={{position:'fixed',top:70,left:80,zIndex:31}}>
-        <div style={{color:'white',fontWeight:700,fontSize:'20px',letterSpacing:-0.5}}>AI Med</div>
+        <img src='2.png' width={129} height={18}></img>
+        {/* <div style={{color:'white',fontWeight:700,fontSize:'20px',letterSpacing:-0.5}}>AI Med</div> */}
       </div>
     </header>
   )
