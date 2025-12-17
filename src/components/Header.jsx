00c87aac15e113572c1a7143e070d278ed130670
miraclesprime <miraclesprime@gmail.com>
2025-12-17 07:20:03 -0500

    deploy fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import logo from './assets/logo.png';
 export default function Header(){
   return (
     <header className="site-header">
       <div className="logo" style={{position:'fixed',top:70,left:80,zIndex:31}}>
-        <img src='/assets/logo.png' width={129} height={18} alt="logo"></img>
+        <img src={logo} width={129} height={18} alt="logo"></img>
       </div>
     </header>
   )
