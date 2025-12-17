242da9cc96b4207e1643f9f5e58f2764cabbb2f4
miraclesprime <miraclesprime@gmail.com>
2025-12-17 06:58:55 -0500

    fixed the deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,68 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/main.jsx',
+  mode: "production",
+
+  entry: "./src/main.jsx",
+
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    publicPath: "/", // ✅ REQUIRED for Netlify
     clean: true,
   },
+
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
+
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             presets: [
-              '@babel/preset-env',
-              ['@babel/preset-react', { runtime: 'automatic' }],
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
             ],
           },
         },
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: "./index.html",
+    }),
+
+    // ✅ Copy Netlify redirects into dist (use .txt source and ignore if missing)
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public/_redirects.txt", to: "_redirects", toType: "file", noErrorOnMissing: true },
+      ],
     }),
   ],
+
   devServer: {
-    static: [
-      {
-        directory: path.join(__dirname, 'public'),
-      },
-      {
-        directory: path.join(__dirname, 'dist'),
-      },
-    ],
+    historyApiFallback: true, // ✅ SPA routing locally
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
     port: 5173,
     hot: true,
-    open: false,
   },
 };
